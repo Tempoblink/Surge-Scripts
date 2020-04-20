4d7ec9cb333ef395eb2fa41ced514ce81c5d5e52
Tempoblink <tempoblink@gmail.com>
2020-04-20 18:19:51 +0800

    add time notification

--- network-changed.js
+++ network-changed.js
@@ -24,6 +24,30 @@ let CELLULAR = 'Rule';
 let NETWORK = $network.wifi.ssid;
 let TAG = false;
 
+//Time format
+Date.prototype.format = function(fmt) { 
+     var o = { 
+        "M+" : this.getMonth()+1,                  
+        "d+" : this.getDate(),                     
+        "h+" : this.getHours(),                   
+        "m+" : this.getMinutes(),                  
+        "s+" : this.getSeconds(),                 
+        "q+" : Math.floor((this.getMonth()+3)/3), 
+        "S"  : this.getMilliseconds()             
+    }; 
+    if(/(y+)/.test(fmt)) {
+            fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length)); 
+    }
+     for(var k in o) {
+        if(new RegExp("("+ k +")").test(fmt)){
+             fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+         }
+     }
+    return fmt; 
+}        
+
+let DATA = $script.startTime.format("yyyy-MM-dd hh:mm:ss");
+
 function changeOutboundMode(is_cellular, mode) {
     if (is_cellular) {
         NETWORK = 'Cellular, '+$network.v4.primaryAddress;
@@ -31,7 +55,7 @@ function changeOutboundMode(is_cellular, mode) {
         NETWORK = 'Wi-Fi, '+NETWORK;
     }
     if($surge.setOutboundMode(mode.toLowerCase()))
-        $notification.post("Outbound Changed!", NETWORK+" Outbound Mode: "+mode, $script.startTime);
+        $notification.post("Outbound Changed!", NETWORK+" Outbound Mode: "+mode, "Start Network: "+DATA);
     $done();
 }
 
