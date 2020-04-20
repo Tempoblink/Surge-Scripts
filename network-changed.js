afbf9fbd8d73d2161c316cd94831eac578e9293b
Tempoblink <tempoblink@gmail.com>
2020-04-20 16:05:17 +0800

    change structure

--- network-changed.js
+++ network-changed.js
@@ -1,6 +1,8 @@
 // event network-changed script-path=network-changed.js
+//version: 2.0
+//auther: tempoblink
 
-let WIFINAME = $network.wifi.ssid;
+let NETWORK = $network.wifi.ssid;
 let IPADDRESS = $network.v4.primaryAddress;
 let WHITENAME = [
             "home_ssid1",
@@ -11,33 +13,37 @@ let BLACKNAME = [
             "free_ssid2"
     ];
 
+let TAG = false;
+let BLACK = 'Direct';
+let WHITE = 'Rule';
 let OTHERS = 'Rule';
 let CELLULAR = 'Rule';
+
+function changeoutbound(TAG, MODE) {
+    if (TAG) {
+        NETWORK = 'Cellular, '+IPADDRESS;
+    }
+    if($surge.setOutboundMode(MODE.toLowerCase()))
+        $notification.post("Outbound Changed!", "Network: "+NETWORK, "Outbound Mode, "+MODE);
+}
 
 //wifi select outbound
 //BLACKNAME select outbound is direct
 //WHITENAME select outbound is rule
 //others you can change by OTHERS = 'direct' or 'rule' or 'global-proxy'
 if ($network.v4.primaryInterface == "en0" && $network.wifi.bssid != 'null') {
-    if (BLACKNAME.indexOf(WIFINAME) != -1) {
-        if($surge.setOutboundMode('direct'))
-            $notification.post("Outbound Changed!", "Network: "+WIFINAME, "Outbound Mode, Direct");
-    }
-    if (WHITENAME.indexOf(WIFINAME) != -1) {
-        if($surge.setOutboundMode('rule'))
-            $notification.post("Outbound Changed!", "Network: "+WIFINAME, "Outbound Mode, Rule");
-    }
-    else {
-        if($surge.setOutboundMode(OTHERS.toLowerCase()))
-            $notification.post("Outbound Changed!", "Network: "+WIFINAME, "Outbound Mode, "+OTHERS);
-    }
+    if (BLACKNAME.indexOf(NETWORK) != -1) 
+        changeoutbound(TAG, BLACK);
+    else if (WHITENAME.indexOf(NETWORK) != -1)
+        changeoutbound(TAG, WHITE);
+    else 
+        changeoutbound(TAG, OTHERS);
 }
 
 
 //cellular select outbound
-if($network.v4.primaryInterface == "pdp_ip0") {
-    if($surge.setOutboundMode(CELLULAR.toLowerCase()))
-        $notification.post("Outbound Changed!", "Network: Cellular, "+IPADDRESS, "Outbound Mode, "+CELLULAR);
-}
+if($network.v4.primaryInterface == "pdp_ip0")
+    TAG = true;
+    changeoutbound(TAG, CELLULAR);
 
 $done();
