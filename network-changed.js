e66607679292aae5c3573e0b725f418c05dd0fab
Tempoblink <tempoblink@gmail.com>
2020-04-20 14:01:30 +0800

    update notification

--- network-changed.js
+++ network-changed.js
@@ -1,16 +1,20 @@
 // event network-changed script-path=network-changed.js
 
-let HOME="home_ssid"
+let WIFINAME = $network.wifi.ssid;
+let IPADDRESS = $network.v4.primaryAddress;
+let WHITENAME = [
+            "home_ssid1",
+            "home_ssid2"
+    ];
 
-if ($network.v4.primaryInterface == 'en0' && $network.wifi.bssid != 'null' && $network.wifi.ssid != HOME) {
+if ($network.v4.primaryInterface == "en0" && $network.wifi.bssid != 'null' && WHITENAME.indexOf(WIFINAME) == -1) {
     if($surge.setOutboundMode('direct'))
-        $notification.post("Outbound Changed!", "Network: $network.wifi.ssid", "Outbound: Direct");
+      $notification.post("Outbound Changed!", "Network: "+WIFINAME+IPADDRESS, "Outbound Mode, Direct");
 } else {
-    if($surge.setOutboundMode('rule')) {
-      if($network.v4.primaryInterface == 'pdp_ip0')
-        $notification.post("Outbound Changed!", "Network: Cellular", "Outbound: Rule");
-      if($network.v4.primaryInterface == 'en0')
-        $notification.post("Outbound Changed!", "Network: "+$network.wifi.ssid, "Outbound: Rule");
-    }
+    if($surge.setOutboundMode('rule'));
+      if($network.v4.primaryInterface == "pdp_ip0")
+        $notification.post("Outbound Changed!", "Network: Cellular"+IPADDRESS, "Outbound Mode, Rule");
+      if($network.v4.primaryInterface == "en0")
+        $notification.post("Outbound Changed!", "Network: "+WIFINAME+IPADDRESS, "Outbound Mode, Rule");
 }
 $done();
