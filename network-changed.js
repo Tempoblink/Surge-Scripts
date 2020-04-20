229897c498f4c23f383a0ee20137c15f7cf3d320
Tempoblink <tempoblink@gmail.com>
2020-04-20 14:56:24 +0800

    add blackname

--- network-changed.js
+++ network-changed.js
@@ -6,15 +6,36 @@ let WHITENAME = [
             "home_ssid1",
             "home_ssid2"
     ];
+let BLACKNAME = [
+            "free_ssid1",
+            "free_ssid2"
+    ];
 
-if ($network.v4.primaryInterface == "en0" && $network.wifi.bssid != 'null' && WHITENAME.indexOf(WIFINAME) == -1) {
-    if($surge.setOutboundMode('direct'))
-      $notification.post("Outbound Changed!", "Network: "+WIFINAME, "Outbound Mode, Direct");
-} else {
-    if($surge.setOutboundMode('rule'));
-      if($network.v4.primaryInterface == "pdp_ip0")
-        $notification.post("Outbound Changed!", "Network: Cellular, "+IPADDRESS, "Outbound Mode, Rule");
-      if($network.v4.primaryInterface == "en0")
-        $notification.post("Outbound Changed!", "Network: "+WIFINAME, "Outbound Mode, Rule");
+let OTHERS = 'rule';
+let CELLULAR = 'rule';
+
+//wifi select outbound
+//BLACKNAME select outbound is direct
+//WHITENAME select outbound is rule
+//others you can change by OTHERS = 'direct' or 'rule' or 'global-proxy'
+if ($network.v4.primaryInterface == "en0" && $network.wifi.bssid != 'null') {
+    if (BLACKNAME.indexOf(WIFINAME) != -1)
+        if($surge.setOutboundMode('direct'))
+            $notification.post("Outbound Changed!", "Network: "+WIFINAME, "Outbound Mode, Direct");
+    if (WHITENAME.indexOf(WIFINAME) != -1)
+        if($surge.setOutboundMode('rule'))
+            $notification.post("Outbound Changed!", "Network: "+WIFINAME, "Outbound Mode, Rule");
+    else {
+        if($surge.setOutboundMode(OTHERS))
+            $notification.post("Outbound Changed!", "Network: "+WIFINAME, "Outbound Mode, Rule");
+    }
 }
+
+
+//cellular select outbound
+if($network.v4.primaryInterface == "pdp_ip0") {
+    if($surge.setOutboundMode(CELLULAR))
+        $notification.post("Outbound Changed!", "Network: Cellular, "+IPADDRESS, "Outbound Mode, Rule");
+}
+
 $done();
